e55870f0de4750fbfc21ff9a960797a645f2ce7c
babi <bobbobbobbob6661@email.com>
2025-05-08 15:52:49 +0530

    created static folder and add an index.htm , also added static folder and inside that created css and js file

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    title = models.charField(max_length=255)
-    duration = models.IntegerField(default=25)
-    completed = models.BooleanField(default=false)
+    title = models.CharField(max_length=255)
+    duration = models.IntegerField(default=25)  
+    completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
